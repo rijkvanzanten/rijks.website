cf4fdd556286c5f41bbd24e3e6776ee444ae2a20
Rijk van Zanten <rijkvanzanten@me.com>
2026-07-30 14:00:37 -0400

    Kill it all

--- app/app.vue
+++
@@ -1,8 +0,0 @@
-<template>
-	<div class="dark:text-white lg:flex font-serif lg:mx-auto lg:w-max lg:items-start py-8">
-		<NuxtRouteAnnouncer />
-
-		<SiteNav class="p-4" />
-		<NuxtPage class="p-4 flex-1 prose dark:prose-invert prose-h1:text-base prose-h2:text-base prose-h2:before:content-['##'] prose-h2:before:mr-2 prose-h3:text-base prose-h4:text-base prose-h5:text-base prose-h6:text-base prose-pre:bg-slate-50 dark:prose-pre:bg-slate-900" />
-	</div>
-</template>
--- app/components/SiteLink.ts
+++
@@ -1,4 +0,0 @@
-export default defineNuxtLink({
-	componentName: "SiteLink",
-	activeClass: "before:content-['→'] font-bold",
-});
