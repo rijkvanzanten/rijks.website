fbe05b4aa63eb21cccd60d60977a4eab58a07086
Rijk van Zanten <rijkvanzanten@me.com>
2026-08-12 23:09:07 -0400

    Simplify build setup

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
-main() {
-    ZOLA_VERSION=0.23.3
-
-    curl -sLJO "https://github.com/getzola/zola/releases/download/v${ZOLA_VERSION}/zola-v${ZOLA_VERSION}-x86_64-unknown-linux-gnu.tar.gz"
-    tar -xf zola-v${ZOLA_VERSION}-x86_64-unknown-linux-gnu.tar.gz
-
-    git submodule update --init --recursive
-
-    ./zola build
-}
-
 set -euo pipefail
+
+ZOLA_VERSION=0.23.3
+
+curl -sLJO "https://github.com/getzola/zola/releases/download/v${ZOLA_VERSION}/zola-v${ZOLA_VERSION}-x86_64-unknown-linux-gnu.tar.gz"
+tar -xf "zola-v${ZOLA_VERSION}-x86_64-unknown-linux-gnu.tar.gz"
+
+git submodule update --init --recursive
+
+./zola build
